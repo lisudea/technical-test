fe010b65f35da11927a0981eb0b545da58059c54
Deivid <datcx@hotmail.com>
2020-02-15 11:09:05 -0500

    Login terminado

---
+++ static/js/index.js
@@ -0,0 +1,15 @@
+var getData=function() {
+
+	var name=document.getElementById("name").value;
+	var contra=document.getElementById("contra").value;
+
+	if(contra==name)
+	{
+		alert("Yaga esta prueba!")
+	}
+	else{
+
+		alert("Los datos no coinciden")
+	}
+}
+
